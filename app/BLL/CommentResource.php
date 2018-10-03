cfb54addf9201cdcbba855777d1a3ed3256b080c
Jovica <writerity.jovica@gmail.com>
2018-10-03 13:01:37 +0200

    Update on comment controller

---
+++ app/BLL/CommentResource.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\BLL;
+
+
+use App\DAL\Comment\CommentRepository;
+use App\DAL\Post\PostRepository;
+use App\Notifications\NotifyPostOwner;
+use BaseTree\Resources\BaseResource;
+use BaseTree\Resources\Contracts\Validations\StoreValidation;
+use Illuminate\Http\Request;
+
+class CommentResource extends BaseResource implements StoreValidation
+{
+	protected $postRepository;
+	
+    public function __construct(CommentRepository $repository, PostRepository $postRepository)
+    {
+        parent::__construct($repository);
+        $this->postRepository = $postRepository;
+    }
+
+	public function store(array $attributes)
+	{
+		$attributes['user_id'] = auth()->id();
+		$attributes['post_id'] = request('post_id');
+		
+		parent::store($attributes);
+
+		$post = $this->postRepository->findWithRelations(request('post_id'), ['user']);
+		$post->user->notify(new NotifyPostOwner($post, auth()->user()->name));
+
+		return redirect()->back();
+	}
+
+	public function storeRules(Request $request = null): array
+	{
+		return [
+			'comment' => 'required|min:4'
+		];
+	}
+}
--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,39 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Comment;
+use App\BLL\CommentResource;
 use App\DAL\Comment\CommentRepository;
-use App\DAL\Post\PostRepository;
+use BaseTree\Controllers\WebController;
 use Illuminate\Http\Request;
-use App\Post;
-use App\Notifications\NotifyPostOwner;
 
-class CommentsController extends Controller
+class CommentsController extends WebController
 {
-	protected $commentRepository;
-	protected $postRepository;
-	
-	public function __construct(CommentRepository $commentRepository, PostRepository $postRepository)
+	public function __construct(CommentResource $resource)
 	{
-		$this->commentRepository = $commentRepository;
-		$this->postRepository = $postRepository;
-	}
-	
-	public function store()
-	{
-		request()->validate([
-			'comment' => 'required|min:4'
-		]);
-		
-		$this->commentRepository->create([
-			'user_id' => auth()->id(),
-			'comment' => request('comment'),
-			'post_id' => request('post_id')
-		]);
-		
-		$post = $this->postRepository->findWithRelations(request('post_id'), ['user']);
-		$post->user->notify(new NotifyPostOwner($post, auth()->user()->name));
-		
-		return redirect()->back();
+		parent::__construct($resource);
 	}
 }
